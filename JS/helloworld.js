42344ca97aad7314f6c831cd465bd11a8a1f4b37
MR.FANDIM <55627341+1FANDIM647@users.noreply.github.com>
2019-12-15 20:34:03 +0300

    Add files via upload

---
+++ JS/helloworld.js
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+
+<body>
+
+    <button onclick="function()">Click here</button>
+    <p id="element1"> Welcome </p>
+
+    <script>
+
+     function function1() {
+
+        document.getElementById('ellement1').innerHTML = "Hello world! "
+
+     }
+
+    </script>
+</body>
+</html>
